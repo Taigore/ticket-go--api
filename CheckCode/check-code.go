98f207621f8e0f4616f5c87c4d0dcb63f325d8fd
Federico Bellia <f.bellia@hotmail.it>
2023-03-05 15:58:06 +0100

    Makes the check endpoint fully JSON

--- CheckCode/check-code.go
+++ CheckCode/check-code.go
@@ -2,22 +2,51 @@ package checkCode
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	appCore "github.com/Taigore/ticket-go/app-core"
 )
 
+type ticketBody struct {
+	TicketNumber string
+}
+
 type validationJson struct {
 	IsValid bool
 }
 
-func Handle(resp http.ResponseWriter, req *http.Request) {
-	validationResult := appCore.CheckTicketCode(" ")
-	validationJson := new(validationJson)
-	validationJson.IsValid = validationResult.IsValid()
+type errorJson struct {
+	Error string
+}
 
+func Handle(resp http.ResponseWriter, req *http.Request) {
+	status, body := handleInner(req)
 	enc := json.NewEncoder(resp)
 
-	resp.WriteHeader(http.StatusOK)
-	enc.Encode(&validationJson)
+	resp.WriteHeader(status)
+	resp.Header().Set("Content-Type", "application/json")
+	enc.Encode(&body)
+}
+
+func handleInner(req *http.Request) (status int, result any) {
+	dec := json.NewDecoder(req.Body)
+	dec.DisallowUnknownFields()
+
+	var body ticketBody
+	err := dec.Decode(&body)
+
+	if err != nil {
+		errorResp := new(errorJson)
+		errorResp.Error = fmt.Sprint(err)
+
+		return http.StatusBadRequest, errorResp
+	}
+
+	validationResult := appCore.CheckTicketCode(body.TicketNumber)
+
+	validationResp := new(validationJson)
+	validationResp.IsValid = validationResult.IsValid()
+
+	return http.StatusOK, validationResp
 }
